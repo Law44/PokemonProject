affbe7517558713bcd6de7bcef8b81e601eed636
David <tlaw4444@gmail.com>
2019-05-28 17:21:26 +0200

    Comenzado diseño jornada

--- app/src/main/java/com/example/pokemonproject/view/JornadaFragment.java
+++ app/src/main/java/com/example/pokemonproject/view/JornadaFragment.java
@@ -1,6 +1,23 @@
 package com.example.pokemonproject.view;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pokemonproject.R;
 
 public class JornadaFragment extends Fragment {
+
+    View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_jornada, container, false);
+        return mView;
+    }
 }
